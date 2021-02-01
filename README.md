d246f1592326d1dacd1413ca571c2a9adc42eeab
森田 昌宏 <m.morita@getgamba.com>
2021-02-01 15:53:54 +0900

    Webクライアントのベータ版デプロイを追加

--- README.md
+++ README.md
@@ -9,16 +9,27 @@ A Slack bot for AWS Lambda that automate deployment with Circle CI.
 1. Slashのワークスペース管理より、Appカスタムインテグレーションへ進み、Slash Commandsを追加します。
 1. Slash Commandsのインテグレーションを追加し、トークンに表示された内容を取得します。
 
-### AWS Lambdaの準備
+### AWS Lambdaの準備(gamba_deploy_bot)
+このLambda関数は、AWS API Gatewayからコマンドを受け取って、別のLambda関数(gamba_deploy_runner)へリクエストを引き継ぎます。
+Githubのプルリクエストの処理は若干時間がかかるため、1つのLambda関数内で一度に処理しようとするとタイムアウトが発生してしまいます。
+
 1. AWS Lambdaに用意されているBluePrintに用意されている、`Slack-echo-command`を元に、lambda関数を作成します。
 1. 以下のコマンドにより、`lambda_function.zip`を作成します
     ```
     $ ./deploy.sh
     ```
 1. AWS Lambdaの関数コードに`lambda_function.zip`をアップロードします
-1. 以下の2つの環境変数を登録します
-    * `githubToken`: Githubのアクセストークン
+1. ハンドラーを`index.slack_handler`に設定します
+1. 以下の環境変数を登録します
     * `slackToken`: Slackの設定で表示されたトークン
+
+### AWS Lambdaの準備(gamba_deploy_runner)
+gamba_deploy_botからリクエストを引き継ぎ、Githubのプルリクエストの処理を実際に行うLambda関数です。
+
+1. 上記で作成した`lambda_function.zip`をアップロードします
+1. ハンドラーを`index.deploy_handler`に設定します
+1. 以下の環境変数を登録します
+    * `githubToken`: Githubのアクセストークン
 
 ### API Gatewayの設定
 1.  `/gamba_deploy_bot - ANY - 統合リクエスト`を開き、マッピングテンプレートを追加します
@@ -39,10 +50,11 @@ Slackのチャネルに対して、以下のコマンドを送ると、デプロ
 ```
 * `branch_name`: デプロイするブランチ名を指定します。省略時は`master`がはいります。
 * `environment`: 以下のいずれかを指定します。
-  * `production`: 本番環境
-  * `staging`: ステージング環境
+  * `production`: 本番環境にデプロイします
+  * `staging`: ステージング環境にデプロイします
   * `app`: iOS/Androidのアプリのビルド＆AppStore/GooglePlayへのアップロード
   * `android`: Androidのアプリのビルド＆GooglePlayへのアップロード 
   * `ios`: iOSのアプリのビルド＆AppStoreへのアップロード 
   * `codepush`: CodePushへのJSコードのアップロード
+  * `web`: `www.getgamba.com/m` 配下にベータ版Webクライアントのみデプロイします
 
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 const request = require('request-promise')
 const querystring = require('querystring')
+const AWS = require('aws-sdk');
+
+const lambda = new AWS.Lambda();
 
 const SLACK_TOKEN = process.env['slackToken']
 const GITHUB_TOKEN = process.env['githubToken']
@@ -37,10 +40,21 @@ const GithubAPI = {
             json: true,
             body: null
         })
-    },
+    }
 }
 
-exports.handler = function(event, context, callback) {
+exports.deploy_handler = function(event, context, callback) {
+    const branch = event['branch']
+    const target = event['target']
+
+    GithubAPI.create_pull_req(branch, target)
+        .then((res)=> GithubAPI.merge_pull_req(res.number))
+        .catch(()=> undefined)
+
+    callback(null)
+}
+
+exports.slack_handler = function(event, context, callback) {
     if (!event['body']) return null
 
     const params = querystring.parse(event['body'])
@@ -48,21 +62,24 @@ exports.handler = function(event, context, callback) {
         return null
 
     const command_text = params['text']
-    let match = command_text.match(/([-_.+0-9a-zA-Z]*) *to +(production|staging|app|android|ios|codepush)/)
+    let match = command_text.match(/([-_.+0-9a-zA-Z]*) *to +(production|staging|app|android|ios|codepush|web)/)
     if (match) {
         let branch = match[1] || 'master'
         let target = match[2]
+        let params = {
+            FunctionName: 'arn:aws:lambda:ap-northeast-1:374317207117:function:gamba_deploy_runner',
+            InvocationType: "Event",
+            Payload: JSON.stringify({ branch, target })
+        }
 
-        GithubAPI.create_pull_req(branch, target)
-            .then((res)=> GithubAPI.merge_pull_req(res.number))
-            .catch(()=> undefined)
-        context.succeed({
+        lambda.invoke(params, (err, data)=> console.log(err, data))
+        callback(null, {
             response_type: 'in_channel',
             text: `これから${branch}を${target}にデプロイしまーす！(๑˃̵ᴗ˂̵) \nhttps://circleci.com/gh/getgamba/gamba\nお疲れ様でしたー＼(^o^)／`,
         })
     } else {
-        context.succeed({
-            text: `意味わかんなーい(≧∀≦)\n/deploy [<branch_name>] to <production|staging|ios|android|app|codepush>\nこんな感じで話しかけてねー♡ `
+        callback(null, {
+            text: `意味わかんなーい(≧∀≦)\n/deploy [<branch_name>] to <production|staging|ios|android|app|codepush|web>\nこんな感じで話しかけてねー♡ `
         })
     }
 }
